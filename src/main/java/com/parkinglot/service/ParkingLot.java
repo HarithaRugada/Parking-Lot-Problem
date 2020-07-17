0e3c2c6d744873551ac7e416aec8f4cd19d2c44e
HarithaRugada <haritharugada@gmail.com>
2020-07-17 19:55:35 +0530

    ADDED : Code to park the vehicle

--- src/main/java/com/parkinglot/service/ParkingLot.java
+++ src/main/java/com/parkinglot/service/ParkingLot.java
@@ -1,4 +1,10 @@
 package com.parkinglot.service;
 
 public class ParkingLot {
+    public Object vehicle;
+
+    public boolean parkVehicle(Object vehicle) {
+        this.vehicle = vehicle;
+        return true;
+    }
 }
