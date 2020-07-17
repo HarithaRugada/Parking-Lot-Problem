4dd930dbbbee9da6ef2828368489cbfca744904b
HarithaRugada <haritharugada@gmail.com>
2020-07-17 20:09:32 +0530

    ADDED : Test case to get true when vehicle is unparked

--- src/test/java/com/parkinglot/test/ParkingLotTest.java
+++ src/test/java/com/parkinglot/test/ParkingLotTest.java
@@ -2,14 +2,28 @@ package com.parkinglot.test;
 
 import com.parkinglot.service.ParkingLot;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+    ParkingLot parkingLot = null;
+    Object vehicle = null;
+
+    @Before
+    public void setUp() {
+        parkingLot = new ParkingLot();
+        vehicle= new Object();
+    }
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue() {
-        ParkingLot parkingLot = new ParkingLot();
-        Object vehicle = new Object();
         boolean isParked = parkingLot.parkVehicle(vehicle);
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLot.parkVehicle(vehicle);
+        boolean isUnParked = parkingLot.unParkVehicle(vehicle);
+        Assert.assertTrue(isUnParked);
     }
 }
