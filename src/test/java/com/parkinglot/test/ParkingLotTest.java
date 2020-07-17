2c4f435b819ad5fca76826ac50c2a1357ee0c6e2
HarithaRugada <haritharugada@gmail.com>
2020-07-17 19:52:47 +0530

    ADDED : Test case to get true when vehicle is parked

--- src/test/java/com/parkinglot/test/ParkingLotTest.java
+++ src/test/java/com/parkinglot/test/ParkingLotTest.java
@@ -1,4 +1,15 @@
 package com.parkinglot.test;
 
+import com.parkinglot.service.ParkingLot;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotTest {
+    @Test
+    public void givenVehicle_WhenParked_ShouldReturnTrue() {
+        ParkingLot parkingLot = new ParkingLot();
+        Object vehicle = new Object();
+        boolean isParked = parkingLot.parkVehicle(vehicle);
+        Assert.assertTrue(isParked);
+    }
 }
