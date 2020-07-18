49b4f9e4d12d332cb6c3e5e5ac3d1f6c9dcd6ff9
HarithaRugada <haritharugada@gmail.com>
2020-07-18 10:13:24 +0530

    ADDED : Test case to check whether the parking is full

--- src/test/java/com/parkinglot/test/ParkingLotTest.java
+++ src/test/java/com/parkinglot/test/ParkingLotTest.java
@@ -1,5 +1,6 @@
 package com.parkinglot.test;
 
+import com.parkinglot.exception.ParkingLotException;
 import com.parkinglot.service.ParkingLot;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,18 +13,31 @@ public class ParkingLotTest {
     @Before
     public void setUp() {
         parkingLot = new ParkingLot();
-        vehicle= new Object();
+        vehicle = new Object();
     }
+
     @Test
-    public void givenVehicle_WhenParked_ShouldReturnTrue() {
+    public void givenVehicle_WhenParked_ShouldReturnTrue() throws ParkingLotException {
         boolean isParked = parkingLot.parkVehicle(vehicle);
         Assert.assertTrue(isParked);
     }
 
     @Test
-    public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
+    public void givenVehicle_WhenUnParked_ShouldReturnTrue() throws ParkingLotException {
         parkingLot.parkVehicle(vehicle);
         boolean isUnParked = parkingLot.unParkVehicle(vehicle);
         Assert.assertTrue(isUnParked);
+    }
+
+    @Test
+    public void givenParkingLot_WhenParkingLotGetFull_ShouldThrowException() {
+        try {
+            parkingLot.parkVehicle(vehicle);
+            Object vehicle2 = new Object();
+            parkingLot.parkVehicle(vehicle2);
+            parkingLot.parkVehicle(new Object());
+        } catch (ParkingLotException e) {
+            Assert.assertEquals("Parking lot is full", e.getMessage());
+        }
     }
 }
