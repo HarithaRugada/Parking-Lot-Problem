374fc6367ac7dc1666f27acbf0425cef57158bb1
HarithaRugada <haritharugada@gmail.com>
2020-07-19 08:20:39 +0530

    REFACTORED : Covering the negative scenarios

--- src/test/java/com/parkinglot/test/ParkingLotTest.java
+++ src/test/java/com/parkinglot/test/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.parkinglot.test;
 
 import com.parkinglot.exception.ParkingLotException;
 import com.parkinglot.service.ParkingLot;
+import com.parkinglot.utility.ParkingOwner;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,14 +13,35 @@ public class ParkingLotTest {
 
     @Before
     public void setUp() {
-        parkingLot = new ParkingLot(1);
+        parkingLot = new ParkingLot(2);
         vehicle = new Object();
     }
 
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue() throws ParkingLotException {
-        boolean isParked = parkingLot.parkVehicle(vehicle);
+        parkingLot.parkVehicle(vehicle);
+        boolean isParked = parkingLot.isVehicleParked(vehicle);
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenParkedTwice_ShouldThrowException() {
+        try {
+            parkingLot.parkVehicle(vehicle);
+            parkingLot.parkVehicle(vehicle);
+        } catch (ParkingLotException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void given2Vehicles_WhenParked_ShouldReturnTrue() throws ParkingLotException {
+        parkingLot.parkVehicle(vehicle);
+        boolean isParked1 = parkingLot.isVehicleParked(vehicle);
+        Object vehicle2 = new Object();
+        parkingLot.parkVehicle(vehicle2);
+        boolean isParked2 = parkingLot.isVehicleParked(vehicle2);
+        Assert.assertTrue(isParked1 && isParked2);
     }
 
     @Test
@@ -30,12 +52,39 @@ public class ParkingLotTest {
     }
 
     @Test
-    public void givenParkingLot_WhenParkingLotGetFull_ShouldThrowException() {
+    public void givenVehicleParked_AndWhenUnParkedAnotherVehicle_ShouldThrowException() {
         try {
             parkingLot.parkVehicle(vehicle);
-            Object vehicle2 = new Object();
-            parkingLot.parkVehicle(vehicle2);
+            boolean isUnParked = parkingLot.unParkVehicle(new Object());
+            Assert.assertFalse(isUnParked);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals("Vehicle not found", e.getMessage());
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenVehicle_CheckIfVehicleIsNotPresent_ShouldThrowException() {
+        try {
+            parkingLot.parkVehicle(null);
+            boolean isParked = parkingLot.isVehicleParked(vehicle);
+            Assert.assertFalse(isParked);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals("Vehicle not found", e.getMessage());
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenVehicle_WhenParkingLotFull_ShouldInformToOwner() {
+        ParkingOwner parkingOwner = new ParkingOwner();
+        parkingLot.registerOwner(parkingOwner);
+        try {
+            parkingLot.parkVehicle(vehicle);
             parkingLot.parkVehicle(new Object());
+            parkingLot.parkVehicle(new Object());
+            boolean parkingFull = parkingOwner.parkingFull();
+            Assert.assertTrue(parkingFull);
         } catch (ParkingLotException e) {
             System.out.println(e.getMessage());
         }
